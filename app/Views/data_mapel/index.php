91ec274b78a73e7bf20f4c236f669e7f79d03e31
rayhanflix <rayhanflix@gmail.com>
2021-10-07 13:19:49 +0700

    test

---
+++ app/Views/data_mapel/index.php
@@ -0,0 +1,176 @@
+<?= $this->extend('template/templateadmin'); ?>
+
+<?= $this->section('content'); ?>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+        <?= $this->include('template/topbar'); ?>
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Data Mapel</h1>
+            </div>
+
+            <div class="container">
+                <!-- Content Row -->
+                <div class="row">
+                    <div class="col">
+                        <a class="btn btn-primary mb-4" id="bayar" hred data-toggle="modal" data-target="#modal">Tambah Mapel</a>
+                        <?php if (session()->getFlashdata('Pesan')) : ?>
+                            <div class="alert alert-success" role="alert">
+                                <?= session()->getFlashdata('Pesan'); ?>
+                            </div>
+                        <?php endif; ?>
+                        <table class="table table-striped" id="data-list">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nama Mapel</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($mapel as $k) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i++; ?></th>
+                                        <td><?= $k['nama_mapel']; ?></td>
+                                        <td>
+                                            <a href="#" class="btn btn-info btn-sm btn-edit fa fa-edit fa-2x" data-id_mapel="<?= $k['id_mapel'] ?>" data-nama_mapel="<?= $k['nama_mapel']; ?>"></a>
+                                            <a href="#" class="btn btn-danger btn-sm btn-delete fa fa-trash fa-2x" data-id_mapel="<?= $k['id_mapel'] ?>"></a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- tambah -->
+                <div class=" modal fade" id="modal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Data Mapel</h5>
+                                <button class="close" data-dismiss="modal">
+                                    <span>&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form id="form" action="<?= base_url(); ?>/mapel/tambahmapel" method="post">
+                                    <div class="form-group">
+                                        <label>Nama Mapel</label>
+                                        <input type="text" class="form-control" name="nama_mapel" id="nama_mapel" required>
+                                    </div>
+                                    <button class="btn btn-success" type="submit">Tambah</button>
+                                    <button class="btn btn-danger" data-dismiss="modal">Close</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- update -->
+
+
+                <form action="/mapel/update" method="post">
+                    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Ubah Data Mapel</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <div class="form-group">
+                                        <label>Nama Mapel</label>
+                                        <input type="hidden" class="id_mapel" name="id_mapel">
+                                        <input type="text" class="form-control nama_mapel" name="nama_mapel" id="nama_mapel" required>
+                                    </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+                <form action="/mapel/delete" method="post">
+                    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Delete Mapel</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <h4>Are you sure want to delete this Mapel?</h4>
+
+                                </div>
+                                <div class="modal-footer">
+                                    <input type="hidden" name="id_mapel" class="id_mapel">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                    <button type="submit" class="btn btn-primary">Yes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End of Main Content -->
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+<script>
+    $('#data-list').DataTable();
+    $(document).ready(function() {
+        
+        $('.btn-edit').on('click', function() {
+            // get data from button edit
+            const id_mapel = $(this).data('id_mapel');
+            const nama_mapel = $(this).data('nama_mapel');
+            // Set data to Form Edit
+            $('.id_mapel').val(id_mapel);
+            $('.nama_mapel').val(nama_mapel);
+
+
+
+            // Call Modal Edit
+            $('#editModal').modal('show');
+        });
+        $('.btn-delete').on('click', function() {
+            // get data from button edit
+            const id_mapel = $(this).data('id_mapel');
+            // Set data to Form Edit
+            $('.id_mapel').val(id_mapel);
+            // Call Modal Edit
+            $('#deleteModal').modal('show');
+        });
+    });
+</script>
+<!-- End of Content Wrapper -->
+<?= $this->endsection(); ?>
